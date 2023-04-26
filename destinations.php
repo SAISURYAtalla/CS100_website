78521adb14e0a6b318c5fd544c8c163378f4ebbb
SAISURYAtalla <tallasai@iitbhilai.ac.in>
2023-04-27 01:26:01 +0530

    new changes1

--- destinations.php
+++ destinations.php
@@ -1,53 +1,60 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Destinations</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Tourism Management System - Destinations</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-	<header>
-		<nav>
-			<ul>
-				<li><a href="index.html">Home</a></li>
-				<li><a href="destinations.php" class="current">Destinations</a></li>
-				<li><a href="packages.php">Packages</a></li>
-				<li><a href="about.php">About Us</a></li>
-				<li><a href="contact.php">Contact Us</a></li>
-			</ul>
-		</nav>
-	</header>
-
-	<main>
-		<section id="destinations">
-			<h2>Top Destinations</h2>
-			<ul>
-				<li>
-					<a href="#">
-						<img src="https://picsum.photos/200/150?random=1" alt="Destination 1">
-						<h3>Destination 1</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pellentesque odio a leo sagittis dignissim.</p>
-					</a>
-				</li>
-				<li>
-					<a href="#">
-						<img src="https://picsum.photos/200/150?random=2" alt="Destination 2">
-						<h3>Destination 2</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pellentesque odio a leo sagittis dignissim.</p>
-					</a>
-				</li>
-				<li>
-					<a href="#">
-						<img src="https://picsum.photos/200/150?random=3" alt="Destination 3">
-						<h3>Destination 3</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pellentesque odio a leo sagittis dignissim.</p>
-					</a>
-				</li>
-			</ul>
+	<div class="header">
+		<h1>Tourism Management System : BOOK MY TRIP</h1>
+	</div>
+	
+	<div class="navbar">
+		<a href="index.html">Home</a>
+		<a href="gallery.php">Gallery</a>
+		<a class="active" href="destinations.php">Destinations</a>
+		<a href="packages.php">Packages</a>
+		<a href="rentals.php">Rentals</a>
+		<a href="contact.php">Contact Us</a>
+	</div>
+	
+	<div class="main">
+		<section id="banner">
+			<h2>Our Destinations</h2>
+			<p>Explore our top destinations and find your next adventure!</p>
 		</section>
-	</main>
-
+		
+		<section id="content">
+			<?php
+				if(isset($_GET['destination'])) {
+					$destination = $_GET['destination'];
+					$display = $destination . '.php';
+					if(file_exists($display)) {
+						include($display);
+					} else {
+						echo '<h3>Sorry, no information available for this destination</h3>';
+					}
+				} else {
+					echo '<p>Please select a destination from the list below to learn more:</p>';
+					echo '<ul>';
+					echo '<li><a href="?destination=paris">Paris, France</a></li>';
+					echo '<li><a href="?destination=tokyo">Tokyo, Japan</a></li>';
+					echo '<li><a href="?destination=sydney">Sydney, Australia</a></li>';
+					echo '</ul>';
+				}
+			?>
+		</section>
+		
+		<section id="about">
+			<h2>About Us</h2>
+			<p>We are a team of experienced travel professionals dedicated to making your vacation dreams a reality. Our platform offers customizable travel packages, expert recommendations on top destinations, and user-friendly booking tools. Let us help you plan your next adventure!</p>
+		</section>
+	</div>
+	
 	<footer>
-		<p>&copy; 2023 Tourism Management System</p>
+		<p>&copy; 2023 Book My Trip. All rights reserved.</p>
 	</footer>
 </body>
 </html>
---
+++ gallery.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Tourism Management System : BOOK MY TRIP</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="header">
+		<h1>Tourism Management System : BOOK MY TRIP</h1>
+	  </div>
+	
+	<div class="navbar">
+    <a href="index.html">Home</a>
+	<a href="gallery.php">Gallery</a>
+    <a href="destinations.php">Destinations</a>
+	<a href="packages.php">Packages</a>
+    <a href="rentals.php">Rentals</a>
+    <a href="contact.php">Contact Us</a>
+    </div>
+	<div class="main">
+		<h2>Gallery</h2>
+		<p>Here are some photos of our top destinations:</p>
+		<div class="gallery">
+			<img src="https://media.istockphoto.com/id/672972564/photo/beautiful-beach-with-palm-trees.jpg?b=1&s=170667a&w=0&k=20&c=W7HqrNedgGsQeCgommH3A2PY0DuU3mLyfj4rk6KX0bQ=" alt="Beach">
+			<img src="https://images.pexels.com/photos/1371360/pexels-photo-1371360.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Thailand">
+			<img src="https://media.istockphoto.com/id/1176439818/photo/making-a-memory.jpg?s=612x612&w=0&k=20&c=QdOhqkmo-9hxfrspVSA0hDdb2W3qITaJgPKqr3J4anc=" alt="Europe">
+			<img src="https://cdn.pixabay.com/photo/2014/09/07/21/52/city-438393_960_720.jpg" alt="City">
+			<img src="https://cdn.pixabay.com/photo/2014/07/30/12/38/colored-pencils-405933_960_720.jpg" alt="Colored pencils">
+			<img src="https://cdn.pixabay.com/photo/2014/09/21/18/56/sunset-455361_960_720.jpg" alt="Sunset">
+		</div>
+        <p>Find more deatils at : Destinations</p>
+	</div>
+	<footer>
+		<p>&copy; 2023 Book My Trip. All rights reserved.</p>
+	</footer>
+</body>
+</html>

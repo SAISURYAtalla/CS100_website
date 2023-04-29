f519c0d5458fba1cfb67a4ce2b58130486dd6246
SAISURYAtalla <tallasai@iitbhilai.ac.in>
2023-04-29 20:13:35 +0530

    contact form updated

--- contact.php
+++ contact.php
@@ -17,25 +17,50 @@
 		<a href="destinations.php">Destinations</a>
 		<a href="packages.php">Packages</a>
 		<a href="rentals.php">Rentals</a>
-		<a href="#">Contact Us</a>
+		<a class="active" href="contact.php">Contact Us</a>
 	</div>
-	<div class="main">
-		<h2>Contact Us</h2>
-		<form action="submit_form.php" method="post">
-			<label for="name">Name:</label>
-			<input type="text" id="name" name="name" required><br>
 
-			<label for="email">Email:</label>
-			<input type="email" id="email" name="email" required><br>
+	<main>
+		<section id="contact">
+			<h2>Contact Us</h2>
+			<p>We'd love to hear from you! You can reach out to us through the following channels:</p>
+			<ul>
+				<li><a href="https://www.facebook.com/bookmytrip">Facebook</a></li>
+				<li><a href="https://www.instagram.com/bookmytrip">Instagram</a></li>
+				<li><a href="https://twitter.com/bookmytrip">Twitter</a></li>
+			</ul>
+			<p>Give any feedback/query if you want:</p>
+			<form method="post" action="submit-feedback.php">
+				<label for="name">Name:</label>
+				<input type="text" id="name" name="name" required>
 
-			<label for="message">Message:</label>
-			<textarea id="message" name="message" required></textarea><br>
+				<label for="email">Email:</label>
+				<input type="email" id="email" name="email" required>
 
-			<input type="submit" value="Submit">
-		</form>
-</div>
-<footer
-	   <p>&copy; 2023 Book My Trip. All rights reserved.</p>
-    </footer>
+				<label for="message">Message:</label>
+				<textarea id="message" name="message" rows="5" required></textarea>
+
+				<button type="submit">Send</button>
+			</form>
+		</section>
+
+		<section id="reviews">
+			<h2>Top Reviews</h2>
+			<ul>
+				<li>"Book My Trip made planning my vacation a breeze! I loved the customized travel packages and the expert recommendations on top destinations."</li>
+				<li>"The user-friendly interface made booking my rental so easy. I especially appreciated the 24/7 customer support for any inquiries."</li>
+				<li>"Thanks to Book My Trip, I had the vacation of a lifetime. I highly recommend this platform to anyone looking to plan their next adventure!"</li>
+			</ul>
+		</section>
+
+		<section id="about">
+			<h2>About Us</h2>
+			<p>We are a team of experienced travel professionals dedicated to making your vacation dreams a reality. Our platform offers customizable travel packages, expert recommendations on top destinations, and user-friendly booking tools. Let us help you plan your next adventure!</p>
+		</section>
+	</main>
+
+	<footer>
+		<p>&copy; 2023 Book My Trip. All rights reserved.</p>
+	</footer>
 </body>
 </html>
